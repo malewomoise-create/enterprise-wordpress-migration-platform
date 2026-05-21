4885fd152975bfa16f37ffaf9a8bcd7e5c98a7ca
Moise Malewo <moisemalewo@Moises-MacBook-Air.local>
2026-05-20 21:20:44 -0400

    Fixed Terraform backend in GitHub Actions

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "enterprise-migration-projet"
-    key            = "wordpress-migration/dev/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile = true
-    encrypt        = true
+    bucket = "your-bucket-name"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
